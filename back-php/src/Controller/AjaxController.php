87b6455696543dc884f6ec5236c37dfe04d77fd4
Jojopok <zaiajonathan@gmail.com>
2023-11-23 15:55:34 +0100

    fix bug database

--- back-php/src/Controller/AjaxController.php
+++ back-php/src/Controller/AjaxController.php
@@ -19,30 +19,30 @@ class AjaxController extends AbstractAPIController
     {
         $articleManager = new ArticleManager();
         $articles = $articleManager->selectAllMovies();
-        return json_encode($articles);
+        return json_encode($articles, JSON_PRETTY_PRINT);
     }
     public function getAllGifts(): string
     {
         $articleManager = new ArticleManager();
         $articles = $articleManager->selectAllGifts();
-        return json_encode($articles);
+        return json_encode($articles, JSON_PRETTY_PRINT);
     }
     public function getAllRecipes(): string
     {
         $articleManager = new ArticleManager();
         $articles = $articleManager->selectAllRecipes();
-        return json_encode($articles);
+        return json_encode($articles, JSON_PRETTY_PRINT);
     }
     public function getAllActivities(): string
     {
         $articleManager = new ArticleManager();
         $articles = $articleManager->selectAllActivities();
-        return json_encode($articles);
+        return json_encode($articles, JSON_PRETTY_PRINT);
     }
     public function getAllItem(): string
     {
         $articleManager = new ArticleManager();
         $articles = $articleManager->selectAllTest();
-        return json_encode($articles);
+        return json_encode($articles, JSON_PRETTY_PRINT);
     }
 }
