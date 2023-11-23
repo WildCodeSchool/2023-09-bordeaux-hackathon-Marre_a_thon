95286dddf6dae7d02fa19513b6151a1a2fd52c5f
Claire Vannier <clairevannierpro@gmail.com>
2023-11-23 17:39:16 +0100

    added button and random movies

--- front-js/src/components/Calendar.jsx
+++ front-js/src/components/Calendar.jsx
@@ -1,10 +1,17 @@
-function Calendar() {
+import Case from "./Case";
+
+function Calendar({ movies, setMovies }) {
   return (
-    <div className="video-background">
-      <video autoPlay loop muted playsInline>
-        <source src="./src/assets/videoCalendar.mp4" type="video/mp4" />
-      </video>
-    </div>
+    <>
+      <div>
+        <Case movies={movies} setMovies={setMovies} />
+      </div>
+      <div className="video-background">
+        <video autoPlay loop muted playsInline>
+          <source src="./src/assets/videoCalendar.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </>
   );
 }
 
