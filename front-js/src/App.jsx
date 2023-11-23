78e9d2bcdd9cefba20b08e1c96baf35e339458a0
Claire Vannier <clairevannierpro@gmail.com>
2023-11-23 13:27:36 +0100

    finish header and footer

--- front-js/src/App.jsx
+++ front-js/src/App.jsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import "./styles/main.scss";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
-  return <></>;
+  return (
+    <div className="container">
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
