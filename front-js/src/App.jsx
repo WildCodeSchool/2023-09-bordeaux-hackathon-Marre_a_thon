6b1ef4c1f02d4f27871bb374a64d70e6d4c76f8a
Claire Vannier <clairevannierpro@gmail.com>
2023-11-23 14:50:18 +0100

    added axios and template fetch

--- front-js/src/App.jsx
+++ front-js/src/App.jsx
@@ -1,8 +1,70 @@
 import "./styles/main.scss";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+// import axios from "axios";
+// import React, { useState } from "react";
 
 function App() {
+  // const [movies, setMovies] = useState([]);
+  // const [recipes, setRecipes] = useState([]);
+  // const [activities, setActivities] = useState([]);
+  // const [gifts, setGifts] = useState([]);
+
+  // useEffect(() => {
+  // const getMovies = () =>
+  //   axios.get("...").then((res) => { setMovies(res.data);
+  // });
+  // getMovies(movies);
+  // }, [setMovies]);
+
+  // return (
+  //   <div>
+  //     <h2>{movies.movie_title}</h2>
+  //     <img src={movies.movie_img} alt="photo du film" />
+  //     <p>{movies.movie_desc}</p>
+  //     <a href={movies.movie_url} />
+  //   </div>
+  // );
+
+  // useEffect(() => {
+  // const getRecipes = () =>
+  //   axios.get("...").then((res) => { setRecipes(res.data);
+  // });
+  // getRecipes(recipes);
+  // }, [setRecipes]);
+  // return (
+  //   <div>
+  //     <h2>{recipies.recipe_title}</h2>
+  //     <img src={recipies.recipe_img} alt="photo du film" />
+  //     <p>{recipies.recipe_description}</p>
+  //     <a href={recipies.recipe_url} />
+  //   </div>
+  // );
+
+  // useEffect(() => {
+  //   axios.get("...").then((res) => setActivities(res.data));
+  // }, []);
+
+  // return (
+  //   <div>
+  //     <h2>{activities.title}</h2>
+  //     <img src={activities.img} alt="photo du film" />
+  //     <p>{activities.description}</p>
+  //     <a href={activities.link} />
+  //   </div>
+  // );
+  // useEffect(() => {
+  //   axios.get("...").then((res) => setGifts(res.data));
+  // }, []);
+  // return (
+  //   <div>
+  //     <h2>{gift.title}</h2>
+  //     <img src={gift.img} alt="photo du film" />
+  //     <p>{gift.description}</p>
+  //     <a href={gift.link} />
+  //   </div>
+  // );
+
   return (
     <div className="container">
       <Header />
