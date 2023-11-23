e05f9ce375b07b80a705c32c9fdca57be5474cbe
Claire Vannier <clairevannierpro@gmail.com>
2023-11-23 15:30:41 +0100

    adapted the video to the body

--- front-js/src/components/Calendar.jsx
+++ front-js/src/components/Calendar.jsx
@@ -1,14 +1,9 @@
-import "../styles/_calendar.scss";
-
 function Calendar() {
   return (
-    <div className="container">
-      <div className="video-background">
-        <h1>hello</h1>
-        <video autoPlay loop muted playsInline>
-          <source src="./src/assets/videoCalendar.mp4" type="video.mp4" />
-        </video>
-      </div>
+    <div className="video-background">
+      <video autoPlay loop muted playsInline>
+        <source src="./src/assets/videoCalendar.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
